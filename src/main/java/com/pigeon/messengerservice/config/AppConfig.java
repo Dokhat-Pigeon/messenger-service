da28aedcef3f9bbebde529e52c55832134cd27c4
dmitry.krivenko <krivenko_da@nlmk.com>
2023-11-20 23:24:28 +0300

    initialize commit

---
+++ src/main/java/com/pigeon/messengerservice/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.pigeon.messengerservice.config;
+
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEurekaClient
+public class AppConfig {
+}
